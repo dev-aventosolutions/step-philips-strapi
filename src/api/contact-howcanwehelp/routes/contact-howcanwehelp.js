2797c1b82a129fd7984cbfb0ed563afa6c73931f
Junaid-Rasheed <junaidrasheed300@gmail.com>
2023-05-31 23:46:32 +0500

    your-home

---
+++ src/api/contact-howcanwehelp/routes/contact-howcanwehelp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-howcanwehelp router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::contact-howcanwehelp.contact-howcanwehelp');
