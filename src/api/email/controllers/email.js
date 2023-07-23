5f55309292649f6354bb86ba52e260416edcd09b
safee <muhammad.safee@codedistrict.com>
2023-07-23 13:00:29 +0500

    saving email to db

--- src/api/email/controllers/email.js
+++ src/api/email/controllers/email.js
@@ -14,12 +14,39 @@ module.exports = createCoreController("api::email.email", ({ strapi }) => ({
         to: ctx.request.body.data.to,
         from: ctx.request.body.data.from, // You can set a default 'from' address here or use the request body.data if needed.
         subject: ctx.request.body.data.subject,
-        text: ctx.request.body.data.message,
-        html: `<h1>Stef & Philips</h1>`,
+        html: `<h1>Stef & Philips</h1>
+        <p>${ctx.request.body.data.html}</p>`,
+        attachments: [
+          {
+            filename: ctx.request.body.data.attachment.filename,
+            path: ctx.request.body.data.attachment.url,
+          },
+        ],
       });
+      await strapi.entityService.create("api::email.email", {
+        data: {
+          to: ctx.request.body.data.to,
+          from: ctx.request.body.data.from,
+          subject: ctx.request.body.data.subject,
+          html: ctx.request.body.data.message,
+          attachment: ctx.request.body.data.attachment.url,
+          status: "sent",
+        },
+      });
+
       return "Email sent successfully!";
     } catch (err) {
       console.log("err>>>>>>>>>...", err);
+      await strapi.entityService.create("api::email.email", {
+        data: {
+          to: ctx.request.body.data.to,
+          from: ctx.request.body.data.from,
+          subject: ctx.request.body.data.subject,
+          html: ctx.request.body.data.message,
+          attachment: ctx.request.body.data.attachment.url,
+          status: "sent",
+        },
+      });
       return "Email not  sent successfully!";
     }
   },
