321143175d563e117346c1cc31ca8de53af94860
safee <muhammad.safee@codedistrict.com>
2023-07-26 22:56:17 +0500

    send-email back to attachments

--- src/api/email/controllers/email.js
+++ src/api/email/controllers/email.js
@@ -16,35 +16,31 @@ module.exports = createCoreController("api::email.email", ({ strapi }) => ({
         html: ctx.request.body.data.html,
       };
       if (ctx?.request?.body?.data?.attachments?.length) {
-        // takes too much time to send the email
-        // ctx?.request?.body?.data?.attachments?.map((file) => {
-        //   mailoptions.attachments = [];
-        //   mailoptions?.attachments?.push({
-        //     filename: file.filename,
-        //     path: file.url,
-        //   });
-        //   return;
-        // });
-
-        const attachmentLinks = ctx.request.body.data.attachments.map(
-          (file) => `<a href="${file.url}" download>${file.filename}</a>`
-        );
-        const attachmentLinksHtml = attachmentLinks.join("<br>");
-        mailOptions.html += `<p>Attachments:</p>${attachmentLinksHtml}`;
+        mailOptions.attachments = [];
+        ctx?.request?.body?.data?.attachments?.map((file) => {
+          mailOptions?.attachments?.push({
+            filename: file.filename,
+            path: file.url,
+          });
+        });
       }
+      // use this if you want to send only the link takes much less time
+      //   const attachmentLinks = ctx.request.body.data.attachments.map(
+      //     (file) => `<a href="${file.url}" download>${file.filename}</a>`
+      //   );
+      //   const attachmentLinksHtml = attachmentLinks.join("<br>");
+      //   mailOptions.html += `<p>Attachments:</p>${attachmentLinksHtml}`;
+      // }
       console.log(mailOptions);
       await strapi.plugin("email").services.email.send(mailOptions);
-      // await strapi.entityService.create("api::email.email", {
-      //   data: {
-      //     to: ctx.request.body.data.to,
-      //     subject: ctx.request.body.data.subject,
-      //     html: ctx.request.body.data.html,
-      //     attachment: ctx.request?.body?.data?.attachment?.url
-      //       ? ctx.request?.body?.data?.attachment?.url
-      //       : "",
-      //     status: "sent",
-      //   },
-      // });
+      await strapi.entityService.create("api::email.email", {
+        data: {
+          to: ctx.request.body.data.to,
+          subject: ctx.request.body.data.subject,
+          html: mailOptions.html,
+          status: "sent",
+        },
+      });
 
       return "Email sent successfully!";
     } catch (err) {
@@ -53,10 +49,7 @@ module.exports = createCoreController("api::email.email", ({ strapi }) => ({
         data: {
           to: ctx.request.body.data.to,
           subject: ctx.request.body.data.subject,
-          html: ctx.request.body.data.html,
-          attachment: ctx.request?.body?.data?.attachment?.url
-            ? ctx.request?.body?.data?.attachment?.url
-            : "",
+          html: mailOptions.html,
           status: "failed",
         },
       });
